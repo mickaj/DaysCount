8bc492615a49e84b2efb173eb3f7ac25299bf4d0
Mic Kajzer <mickaj@mkajzer.pl>
2019-03-24 18:40:34 +0100

    Bootstrapper configured to use SimpleContainer

--- WpfUI/Bootstrapper.cs
+++ WpfUI/Bootstrapper.cs
@@ -11,6 +11,8 @@ namespace WpfUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
@@ -19,6 +21,21 @@ namespace WpfUI
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
