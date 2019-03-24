a24fee3da52d86367fe09641f2b7a26e84f40898
Mic Kajzer <mickaj@mkajzer.pl>
2019-03-24 19:50:47 +0100

    EventHandlerBase: string rephrased

--- WpfUI/EventHandlerBase.cs
+++ WpfUI/EventHandlerBase.cs
@@ -7,7 +7,7 @@ namespace WpfUI
         protected string DateFormat = "dd-MM-yyyy";
         protected CultureInfo CultureInfo = CultureInfo.InvariantCulture;
 
-        protected string FILE_DOES_NOT_EXIST_EXCEPTION_MESSAGE = "XML file with Event data doesn't exists!";
+        protected string FILE_DOES_NOT_EXIST_EXCEPTION_MESSAGE = "XML file with Event data does not exists!";
         protected string FILE_DOES_NOT_CONTAIN_VALID_DATA_EXCEPTION_MESSAGE = "XML file does not contain valid Event data!";
 
         protected string XmlString = "<?xml version=\u00221.0\u0022 encoding=\u0022utf-8\u0022 ?>"
